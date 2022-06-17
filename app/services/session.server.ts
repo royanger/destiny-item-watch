be9173ccbc2630dba5b47766a9595e16aaffd355
Roy Anger <roy@royanger.com>
2022-06-16 20:15:16 -0400

    Finished setting up Remix, testing deployment works and auth works live

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,15 +1,15 @@
-import { createCookieSessionStorage } from '@remix-run/node';
+import { createCookieSessionStorage } from '@remix-run/node'
 
 export let sessionStorage = createCookieSessionStorage({
    cookie: {
       name: 'destinyitemwatch',
       sameSite: 'lax',
-      domain: process.env.CALLBACK_URL,
+      domain: process.env.URL,
       path: '/',
       httpOnly: true,
       secrets: ['process.env.SECRET'],
       secure: process.env.NODE_ENV === 'production',
    },
-});
+})
 
-export let { getSession, commitSession, destroySession } = sessionStorage;
+export let { getSession, commitSession, destroySession } = sessionStorage
